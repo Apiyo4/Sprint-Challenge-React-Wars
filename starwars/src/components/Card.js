acca6c163f5638c3a674ce133a77ed8e0891dab6
Apiyo4 <odingoannette@gmail.com>
2019-11-08 13:58:24 +0300

    Change color of ReactWars heading and capitalize the first letters of data received

--- starwars/src/components/Card.js
+++ starwars/src/components/Card.js
@@ -2,7 +2,7 @@ import React from 'react';
 import Styled from 'styled-components';
 
 const Div = Styled.div`
-color: #443e3e;
+
 width: 46%;
 box-shadow: 1px 1px #5b0909, -0.5em -0.2em 0.4em #5b0909;
 margin-top : 6%;
@@ -10,6 +10,7 @@ font-size: 24px;
 `
 const P = Styled.p`
     font-size: 32px;
+    text-transform: capitalize;
 `;
 
 function Cards(props){
